a64c0597df013a4f240d5561e594f5eaa8311731
Zixuan <davidzxyang@gmail.com>
2020-10-01 21:26:25 -0500

    add post method

--- db/db_test/db.js
+++ db/db_test/db.js
@@ -2,18 +2,18 @@ const cassandra = require('cassandra-driver');
 var express = require('express');
 var app = express();
 
+const client = new cassandra.Client({
+  contactPoints: ['cassandra-test'],
+  localDataCenter: 'datacenter1',
+  keyspace: 'pimin_net'
+});
+
 app.get("/cassandra/v1", function(req, res) {
   if(req.query.id == null) {
     console.log("Parameter Error! Missing 'id'!");
     res.send("Parameter Error! Missing 'id'!");
     return;
-}
-
-  const client = new cassandra.Client({
-    contactPoints: ['10.176.67.88:9042'],
-    localDataCenter: 'datacenter1',
-    keyspace: 'pimin_net'
-  });
+  } 
 
   const query = 'select * from users where id = ?';
   const param = [req.query.id];
@@ -22,6 +22,24 @@ app.get("/cassandra/v1", function(req, res) {
   .then(function(result) {
     console.log('Result: ', result.rows);
     res.send(result.rows);
+  })  
+})
+
+app.post("/cassandra/v1", function(req, res) {
+  var jsonStr;
+  if(req.body.data)
+  {
+      console.log(req.body.data);
+      jsonStr = req.body.data;
+  }
+
+  const query = 'insert into users (id, user_name) values (?, ?)';
+  const param = [jsonStr.id, jsonStr.user_name];
+
+  client.execute(query, param, { prepare: true })
+  .then(function(result) {
+    console.log('Result: ', result);
+    res.send(result);
   })  
 })
 
