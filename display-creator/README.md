1343c9c4b5e8b1b4c99b4417f2c8184477072d7e
Alex Neal <alex@alexneal.net>
2020-10-09 16:46:56 -0500

    added display creator

---
+++ display-creator/README.md
@@ -0,0 +1,5 @@
+ # Parking Lot Display Generator
+
+ Example Request
+
+ curl --header "Content-Type: application/json" --request POST --data '[{"licenseNumber": "ABC-1234","parkingSlotType":"green"},{"licenseNumber":"CCC1234","parkingSlotType":"orange"}]' localhost:5000
---
+++ display-creator/parkinglot-display.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+import math
+
+def barchart(data):
+    dot = '∎'
+    total = sum(data.values())
+
+    
+    max_length = max([len(key) for key in data.keys()])
+    max_length = min(max_length, 50)
+    value_characters = 80 - max_length
+    max_value = max(data.values())
+    scale = int(math.ceil(float(max_value) / value_characters))
+    scale = max(1, scale)
+    
+    output = "\nOCCUPIED SPACES\n"
+    output += "%d vehicles in parking lot, " % total
+    output += "each " + dot + " represents a count of %d.\n\n" % scale
+    
+    
+    str_format = "%" + str(max_length) + "s [%6d] %s%s\n"
+    percentage = ""
+    for key, value in data.items():
+        # Uncomment to display percentages
+        # percentage = " (%0.2f%%)" % (100 * value / total)
+        output += str_format % (key[:max_length], value, int((value / scale)) * dot, percentage)
+        
+    return output + '\n'
+
+
+app = Flask(__name__)
+
+@app.route('/is-alive', methods=['GET'])
+def is_alive():
+    return jsonify({"alive": True})
+
+
+@app.route('/', methods=['POST'])
+def index():
+    slot_type_counts = {}
+    for vehicle in request.json:
+        if vehicle['parkingSlotType'] in slot_type_counts:
+            slot_type_counts[vehicle['parkingSlotType']] += 1
+        else:
+            slot_type_counts[vehicle['parkingSlotType']] = 1
+    
+    return barchart(slot_type_counts)
