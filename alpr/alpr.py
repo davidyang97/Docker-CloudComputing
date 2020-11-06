58ad80d425d1ace786bdf44d8174550eabbe4ae7
sethbedford <48502695+sethbedford@users.noreply.github.com>
2020-11-06 04:12:56 -0600

    Update alpr.py

--- alpr/alpr.py
+++ alpr/alpr.py
@@ -5,21 +5,22 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/plate', methods=['POST'])
+@app.route('/process', methods=['POST'])
 def plate():
-        SECRET_KEY = 'sk_dd5891bbd0e246f345e7d1ac'
+    SECRET_KEY = 'sk_dd5891bbd0e246f345e7d1ac'
 
-        img_base64 = request.form.get('img')
+    #img_base64 = request.form.get('img')
+    input = request.json
+    url = 'https://api.openalpr.com/v3/recognize_bytes?recognize_vehicle=1&country=us&secret_key=%s' % (SECRET_KEY)
+    r = requests.post(url, data = input['img'])
 
-        url = 'https://api.openalpr.com/v3/recognize_bytes?recognize_vehicle=1&country=us&secret_key=%s' % (SECRET_KEY)
-        r = requests.post(url, data = img_base64)
-
-        data = r.json()
-        output = '{"plate": "' + str(data['results'][0]['plate']) + '", "confidence": "' + str(data['results'][0]['confidence']) + '"}'
-        output = json.loads(output)
-        return output
+    data = r.json()
+    output = '{"plate": "' + str(data['results'][0]['plate']) + '", "confidence": "' + str(data['results'][0]['confidence']) + '"}'
+    output = json.loads(output)
+    input.update(output)
+    return input
 
 @app.route('/is-alive', methods=['GET'])
 def alive():
-        out = '{"alive": "true"}'
-        return json.loads(out)
+    out = '{"alive": "true"}'
+    return json.loads(out)
